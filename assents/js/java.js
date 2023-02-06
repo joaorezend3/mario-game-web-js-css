8f25114d8e966364f824adffba93dc919ac25ff6
joaoerzend3 <joaovitorezende45@gmail.com>
2023-02-06 20:15:35 -0300

    aviso moba 🚀

--- assents/js/java.js
+++ assents/js/java.js
@@ -1,8 +1,8 @@
-// let alertShow = false;
-// if (!alertShow) {
-//   alert("Clicke qualquer tecla de para pular ou precione no botão 'JUMP'")
-//   alertShow = true;
-// };
+let alertShow = false;
+if (!alertShow) {
+  alert("Clicke qualquer tecla de para pular ou precione no botão 'JUMP' Essa simulação não está responsiva para dispositiovos menores.");
+  alertShow = true;
+};
 const display = document.querySelector("#contagem");
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
